2291540aedbdcc2f9e0c6391d2a9bc944a77ec78
moviesDude <133237308+moviesdude@users.noreply.github.com>
2023-05-13 11:12:58 +0530

    Update info.py

--- info.py
+++ info.py
@@ -32,7 +32,7 @@ AUTH_GROUPS = [int(ch) for ch in auth_grp.split()] if auth_grp else None
 
 # MongoDB information
 DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://Mdbots:Mdbots@mdbots.kexwbv7.mongodb.net/?retryWrites=true&w=majority")
-DATABASE_NAME = environ.get('DATABASE_NAME', "MD filter")
+DATABASE_NAME = environ.get('DATABASE_NAME', "MDfilter")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 # Others
